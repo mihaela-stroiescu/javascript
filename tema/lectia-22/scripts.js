2c5dfb03dda2929ca9843df3c9aa9b882a02dc98
Mihaela Stroiescu <mihaela@projects-abroad.net>
2018-03-01 17:37:15 +0200

    tema 22

---
+++ tema/lectia-22/scripts.js
@@ -0,0 +1,12 @@
+//$('#position').click(randomPosition);
+
+function getRandomNumber(min, max) {
+    const randomNumber = Math.floor(min + Math.random() * (max - min));
+    return randomNumber;
+};
+console.log(getRandomNumber(1, 10));
+
+const randomPosition = () => {
+    const randomTop = randomNumber.css({top: randomTop + 'px'});
+
+};
