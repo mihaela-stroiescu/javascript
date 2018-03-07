b5d81e5b45d8ea8b3fef7d1eda302c003a14ff6b
Mihaela Stroiescu <mihaela@projects-abroad.net>
2018-03-07 17:19:45 +0200

    tema 23 cont

--- tema/lectia-23/scripts.js
+++ tema/lectia-23/scripts.js
@@ -1,35 +1,7 @@
 
-function getRandomNumber(min, max) {
-    const randomNumber = Math.floor(min + Math.random() * (max - min));
-    return randomNumber;
-};
 
-const randomPosition = () => {
-    const randomTop = getRandomNumber(0, $('.display').height() - $('.fas').height());
-    const randomLeft = getRandomNumber(0, $('.display').width() - $('.fas').width());
-    $('.fas').css({ top: randomTop + 'px', left: randomLeft + 'px' });
 
-};
 
-$('#position').click(randomPosition);
-
-const randomColor = () => {
-    const red = getRandomNumber(0, 255);
-    const blue = getRandomNumber(0, 255);
-    const green = getRandomNumber(0, 255);
-    //return $('.fas').css({ color: 'rgb(' +  red  + ', ' +  green + ', ' +  blue  + ')'});
-    return $('.fas').css({ color: `rgb(${red}, ${green}, ${blue})`});
-};
-
-$('#color').click(randomColor);
-
-const randomSize = () => {
-    const fontSize = getRandomNumber(10, 50);
-    $('.fas').css({'font-size': fontSize + 'px'});
-    return;
-};
-
-$('#size').click(randomSize);
 
 
 
