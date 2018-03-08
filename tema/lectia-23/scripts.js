021f3a8d0d327b31342a6c36174e883db3349ea2
mihaela-stroiescu <mihaela@projects-abroad.net>
2018-03-08 23:35:30 +0200

    tema 23

--- tema/lectia-23/scripts.js
+++ tema/lectia-23/scripts.js
@@ -1,7 +1,15 @@
+$(() => {
+    GetNumbers();
+});
 
+function ReadInput() {
+    var textInput = $('.input').val();
+    return textInput;
+}
 
-
-
-
-
-
+function GetNumbers() {
+    $('.button').click(function() {
+        ReadInput();
+        console.log(ReadInput());
+    });
+}
