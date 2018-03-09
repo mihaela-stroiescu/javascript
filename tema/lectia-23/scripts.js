730be4bc4faa01c7aba46c8bcdfb822adbc17380
Mihaela Stroiescu <mihaela@projects-abroad.net>
2018-03-09 10:49:50 +0200

    tema 23

--- tema/lectia-23/scripts.js
+++ tema/lectia-23/scripts.js
@@ -1,3 +1,31 @@
+$(() => {
+    GetValue();
+});
+
+const GetValue = () => {
+   const valor = $('#add').val();
+    GetValue(valor);
+};
+console.log(GetValue);
+//$(() => {
+//    setRandomEvent();
+//});
+
+//const setRandomEvent = () => {
+//    $('.buton').click();
+//};
+
+//$(() => {
+//    $('#add').keyup(function () {
+//        takeValue($('#add').val());
+//    });
+
+//});
+
+//$(() => {
+//    setRandomEvent();
+//});
+
 
 
 
